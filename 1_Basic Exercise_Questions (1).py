90c34894b73d87b5baea644dcbfe1f55de738f52
satish-0007 <162107684+satish-0007@users.noreply.github.com>
2024-03-04 14:37:44 +0530

    Add files via upload

---
+++ 1_Basic Exercise_Questions (1).py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Sample Question print - sachin t - shirt number is 10
+name = "sachin"
+t_shirt = 10
+# Your Answer
+
+print(name,"t-shirt number is",t_shirt)
+
+
+# In[2]:
+
+
+# 1 . Question | print - India is number 1 team and has won 2012 worldcup and it rank 1 and the captain name is kholi
+team = "india"
+worldcup = 2012
+captain = "kholi"
+rank = 1
+print(team,"is number",rank,"team and has won",worldcup,"worldcup and it rank",rank,"and the captain name is",captain)
+
+
+
+
+
+
+
+
+# In[2]:
+
+
+# 2 . Question |print the datatype of the x,y,z
+
+x = 56
+y = 56.78
+z = "python"
+print(x)
+print(y)
+print(z)
+
+
+
+
+
+
+
+# In[3]:
+
+
+# 3 . Question | write a program to print 10.0 using typecasting
+
+a = 10
+
+# Your Answer
+b=float(a)
+print(b)
+
+
+
+
+
+# In[5]:
+
+
+# 4 . Question | write a program to print 12 using typecasting
+
+a = 12.56
+
+
+b=int(a)
+print(b)
+
+
+
+# In[6]:
+
+
+# 5 . write a program print 30 by using typecasting conncept
+
+a = "10"
+b = "20"
+a=int(a)
+b=int(b)
+print(a+b)
+
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
